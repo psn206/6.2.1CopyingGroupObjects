1831d9116ba878bf074808648f8b6442c50e1ad1
Sergei Prutsakov <psn206@mail.ru>
2024-09-13 09:21:13 +0300

    Добалено решение по части 2

--- CopyingGroupObjects/Main.cs
+++ CopyingGroupObjects/Main.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using System;
@@ -15,21 +16,86 @@ namespace CopyingGroupObjects
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            UIApplication uiApp = commandData.Application;
-            UIDocument uiDoc = uiApp.ActiveUIDocument;
-            Document doc = uiDoc.Document;
+            try
+            {
+                UIApplication uiApp = commandData.Application;
+                UIDocument uiDoc = uiApp.ActiveUIDocument;
+                Document doc = uiDoc.Document;
+                GroupPickFilter filter = new GroupPickFilter();
 
-            Reference reference = uiDoc.Selection.PickObject(ObjectType.Element, "Выберет группу объектов");
-            Element element = doc.GetElement(reference);
-            Group group = element as Group;
-            XYZ point = uiDoc.Selection.PickPoint("Выберте точку вставки");
+                Reference reference = uiDoc.Selection.PickObject(ObjectType.Element, filter,"Выберет группу объектов");
+                Element element = doc.GetElement(reference);
+                Group group = element as Group;
 
-            Transaction ts = new Transaction(doc,"Копирование группы объеетов");
-            ts.Start();
-            doc.Create.PlaceGroup(point,group.GroupType);
-            ts.Commit();
+                XYZ groupCenter = GetElementCenter(element);
+                Room room = GetRoomByPoint(doc, groupCenter);
+                XYZ roomCenter = GetElementCenter(room);
+                XYZ offset = groupCenter - roomCenter;
+
+                XYZ point = uiDoc.Selection.PickPoint("Выберте точку вставки");
+                Room RoomInstallation = GetRoomByPoint(doc, point);
+                XYZ RoomInstallationCenter = GetElementCenter(RoomInstallation);
+                XYZ installationPoint = RoomInstallationCenter + offset;
+
+                Transaction ts = new Transaction(doc, "Копирование группы объеетов");
+                ts.Start();
+                doc.Create.PlaceGroup(installationPoint, group.GroupType);
+                ts.Commit();
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
 
             return Result.Succeeded;
+        }
+        public XYZ GetElementCenter(Element element)
+        {
+            BoundingBoxXYZ bounding = element.get_BoundingBox(null);
+            return (bounding.Max + bounding.Min) / 2;
+        }
+        public Room GetRoomByPoint(Document doc, XYZ point)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+            foreach(Element element in collector)
+            {
+                Room room = element as Room;
+                if(room != null)
+                {
+                    if (room.IsPointInRoom(point))
+                    {
+                        return room;
+                    }
+                }               
+            }
+            return null;
+        }
+
+    }
+
+    public class GroupPickFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            if (elem.Category.Id.IntegerValue == (int)BuiltInCategory.OST_IOSModelGroups)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
         }
     }
 }
